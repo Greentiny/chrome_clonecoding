de92e1894474c09afdddbe381a9c8c13d4f11f0c
Greentiny <139324498+Greentiny@users.noreply.github.com>
2025-04-09 20:52:24 +0900

    Add files via upload

--- js/background.js
+++ js/background.js
@@ -4,7 +4,7 @@
 // bgImage.src=`img/${chosenImage}`;
 // document.body.appendChild(bgImage);
 
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,4 @@
-// function onGeoOk(position) {
+ // function onGeoOk(position) {
 //   const lat = position.coords.latitude;
 //   const lng = position.coords.longitude;
 //   console.log("You live in", lat, lng);
@@ -13,19 +13,15 @@ const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
 const API_KEY = "4e91e83bb01e540f2a0eab95f68cbb8c";
 
-function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-    });
-}
-function onGeoError() {
-  alert("Can't find you. No weather for you.");
-}
+// 우만동 고정 좌표
+const lat = 37.2830;
+const lon = 127.0467;
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    city.innerText = "우만동";
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+  });
